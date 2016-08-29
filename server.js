56bc02968bdbcf91a53e8686e8b5b960cafd9363
Michael Meisels <mmeisels@salesforce.com>
2016-08-29 13:39:03 +1000

    Added new Theme to it

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
-var app = require('http').createServer(handler)
-var io = require('socket.io')(app);
 var fs = require('fs');
+var express = require('express');
+var app = express();
+var server = app.listen(process.env.PORT || 8080);
+var io      = require('socket.io').listen(server);
 
 var request = require('request');
 var GtfsRealtimeBindings = require('gtfs-realtime-bindings');
@@ -25,22 +27,7 @@ var oauth2 = require('simple-oauth2')(credentials);
 // Get the access token object for the client
 oauth2.client.getToken(tokenConfig, saveToken);
 
-
-app.listen(process.env.PORT || 8080);
-
-function handler (req, res) {
-  fs.readFile(__dirname + '/index.html',
-  function (err, data) {
-    if (err) {
-      res.writeHead(500);
-      return res.end('Error loading index.html');
-    }
-
-    res.writeHead(200);
-    res.end(data);
-  });
-}
-
+app.use(express.static(__dirname + '/public'));
 
 
 // Save the access token
@@ -83,11 +70,46 @@ io.on('connection', function (socket) {
           }
           if (entity.vehicle) {
                 if (entity.vehicle.position) {
+                  //console.log('Access Token Error');
                     socket.emit('vehicle',{route: entity.vehicle.trip.route_id, vehicle: entity.vehicle.trip.trip_id, name: entity.vehicle.trip.trip_id, key: entity.vehicle.trip.trip_id, lat:entity.vehicle.position.latitude,lng:entity.vehicle.position.longitude });
                 }
             }
         });
     })
+
+    // var requestSettings2 = {
+    //     method: 'GET',
+    //     url: 'https://api.transport.nsw.gov.au/v1/gtfs/vehiclepos/ferries',
+    //     encoding: null,
+    //     'auth': {
+    //         'bearer': token.token.access_token
+    //     }
+    // };
+    //
+    // request.get(requestSettings2, function(err, res, body) {
+    //     if (err) {
+    //         console.log('Access Token Error', error.message);
+    //         console.log(error);
+    //     };
+    //     var feed2 = GtfsRealtimeBindings.FeedMessage.decode(body);
+    //     feed2.entity.forEach(function(entity){
+    //       if (entity.alert) {
+    //       //   console.log('Route Alert: ' + entity.alert.cause);
+    //       //   console.log('Route effect: ' + entity.alert.effect);
+    //       //     console.log('Route header_text: ' + entity.alert.header_text);
+    //       }
+    //       if (entity.trip_update) {
+    //         //console.log('Route Alert: ' + entity.trip_update.delay);
+    //
+    //       }
+    //       if (entity.vehicle) {
+    //             if (entity.vehicle.position) {
+    //               //console.log('Access Token Error');
+    //                 socket.emit('ferry',{route: entity.vehicle.trip.route_id, vehicle: entity.vehicle.trip.trip_id, name: entity.vehicle.trip.trip_id, key: entity.vehicle.trip.trip_id, lat:entity.vehicle.position.latitude,lng:entity.vehicle.position.longitude });
+    //             }
+    //         }
+    //     });
+    // })
 
     var requestSettings1 = {
         method: 'GET',
@@ -103,8 +125,8 @@ io.on('connection', function (socket) {
             console.log('Access Token Error', error.message);
             console.log(error);
         };
-        var feed = GtfsRealtimeBindings.FeedMessage.decode(body);
-        feed.entity.forEach(function(entity){
+        var feed1 = GtfsRealtimeBindings.FeedMessage.decode(body);
+        feed1.entity.forEach(function(entity){
           if (entity.alert) {
             console.log('Route Alert: ' + entity.alert.cause);
             console.log('Route effect: ' + entity.alert.effect);
