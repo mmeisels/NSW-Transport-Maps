0290af5795da10a1cd56698b684fd748e995c010
Michael Meisels <mmeisels@salesforce.com>
2016-08-30 14:06:12 +1000

    Trying Postgres install

--- config/config.js
+++ config/config.js
@@ -1,8 +1,7 @@
 module.exports={
-  "facebook_api_key"      :     "FB APP ID",
-  "facebook_api_secret"   :     "FB API SECRET",
-  "callback_url"          :     "http://localhost:3000/auth/facebook/callback",
+  "facebook_api_key"      :     "173240016435994",
+  "facebook_api_secret"   :     "857a160d6ef842cf7af72bcf9d547e00",
+  "callback_url"          :     "http://127.0.0.1:8080/auth/facebook/callback",
   "use_database"          :     "true",
   databaseURL: process.env.DATABASE_URL || "postgres://@127.0.0.1:5432/nwsbus"
-
 }
