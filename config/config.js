3f3f1159815af7a7df244ee5d2a7e908c76b1773
Michael Meisels <mmeisels@salesforce.com>
2016-08-30 14:36:33 +1000

    Removed Mongoose

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 module.exports={
   "facebook_api_key"      :     "173240016435994",
   "facebook_api_secret"   :     "857a160d6ef842cf7af72bcf9d547e00",
-  "callback_url"          :     "https://nswbus-pr-2.herokuapp.com/auth/facebook/callback",
+  "callback_url"          :     "https://nswbus-staging-pr-2.herokuapp.com/auth/facebook/callback",
   "use_database"          :     "true",
   databaseURL: process.env.DATABASE_URL || "postgres://@127.0.0.1:5432/nwsbus"
 }
